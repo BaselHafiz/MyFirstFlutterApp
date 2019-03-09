32e71d9fbd29c44e7b3894161659f2f8eceb1dfe
Basel <basselhafiz@gmail.com>
2019-03-09 16:43:02 +0200

    7.3. Configuring Text Fields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProductCreatePage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _ProductCreatePageState();
   }
 }
 
-class _ProductCreatePageState extends State<ProductCreatePage>{
-
-  String _titleValue = '';
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String _title = '';
+  String _description = '';
+  double _price = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +19,26 @@ class _ProductCreatePageState extends State<ProductCreatePage>{
         TextField(
           onChanged: (String value) {
             setState(() {
-              _titleValue = value;
+              _title = value;
             });
           },
         ),
-        Text(_titleValue),
+        TextField(
+          maxLines: 3,
+          onChanged: (String value) {
+            setState(() {
+              _description = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              _price = double.parse(value);
+            });
+          },
+        ),
       ],
     );
   }
