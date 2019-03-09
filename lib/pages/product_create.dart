01f4e7b55d44c8c3e512e857dba431c43862e9c2
Basel <basselhafiz@gmail.com>
2019-03-09 15:43:11 +0200

    7. Handling User Input 7.2. Saving User Input

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage>{
+
+  String _titleValue = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        color: Theme.of(context).primaryColor,
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text('This is a modal'),
-                );
-              });
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              _titleValue = value;
+            });
+          },
+        ),
+        Text(_titleValue),
+      ],
     );
   }
 }
