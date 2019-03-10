ebacee3b9992b38e84b12689f0bd12ccd460e014
Basel <basselhafiz@gmail.com>
2019-03-10 20:24:50 +0200

    7.10. Adding a SwitchListTile

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,19 +1,67 @@
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _email = '';
+  String _password = '';
+  bool _acceptTerms = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Login'),
         ),
-        body: Center(
-          child: RaisedButton(
-              child: Text("Login"),
-              color: Theme.of(context).primaryColor,
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/productsPage');
-              }),
+        body: Container(
+          margin: EdgeInsets.all(15),
+          child: ListView(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Email'),
+                onChanged: (String value) {
+                  setState(() {
+                    _email = value;
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                onChanged: (String value) {
+                  setState(() {
+                    _password = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SwitchListTile(
+                value: _acceptTerms,
+                title: Text('Accept Terms'),
+                onChanged: (bool value) {
+                  setState(() {
+                    _acceptTerms = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RaisedButton(
+                  child: Text("Login"),
+                  textColor: Colors.white,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/productsPage');
+                  }),
+            ],
+          ),
         ));
   }
 }
