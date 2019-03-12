f30c7fec9b73052d677c2b7d45be5a53610942cf
Basel <basselhafiz@gmail.com>
2019-03-12 18:46:38 +0200

    8.8. Setting Borders

--- lib/products.dart
+++ lib/products.dart
@@ -45,6 +45,15 @@ class Products extends StatelessWidget {
               ],
             ),
           ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey, width: 2),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            margin: EdgeInsets.only(top: 7),
+            padding: EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+            child: Text('Union Square, San Fransisco'),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
