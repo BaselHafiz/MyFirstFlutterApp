a3001813da63e6b3f931c5a43ce33b31c7f8cdf8
Basel <basselhafiz@gmail.com>
2019-04-06 18:09:06 +0200

    12.11. Using Pull to Refresh

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -50,7 +50,11 @@ class _ProductsPageState extends State<ProductsPage> {
         } else if (model.isLoading) {
           content = Center(child: CircularProgressIndicator());
         }
-        return content;
+        return RefreshIndicator(
+          color: Colors.deepOrange,
+          child: content,
+          onRefresh: model.fetchProducts,
+        );
       },
     );
   }
