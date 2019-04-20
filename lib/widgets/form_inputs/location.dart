3720756236059baf40ddfa7f70ea19971aac5c8b
Basel <basselhafiz@gmail.com>
2019-04-20 20:20:24 +0200

    14.6,7 Adding Geocoding & Maps with Real Coordinates

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../helpers/ensure_visible.dart.dart';
 import 'package:map_view/map_view.dart';
 import 'package:map_view/static_map_provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -13,11 +15,11 @@ class LocationInput extends StatefulWidget {
 class _LocationInputState extends State<LocationInput> {
   final FocusNode _addressInputFocusNode = FocusNode();
   Uri _staticMapUri;
+  final TextEditingController _addressInputController = TextEditingController();
 
   @override
   void initState() {
     _addressInputFocusNode.addListener(_updateLocation);
-    getStaticMap();
     super.initState();
   }
 
@@ -27,18 +29,36 @@ class _LocationInputState extends State<LocationInput> {
     super.dispose();
   }
 
-  void _updateLocation() {}
+  void _updateLocation() {
+    if (!_addressInputFocusNode.hasFocus) {
+      getStaticMap(_addressInputController.text);
+    }
+  }
 
-  void getStaticMap() {
+  void getStaticMap(String address) async {
+    if (address.isEmpty) {
+      return;
+    }
+    final Uri uri = Uri.https(
+      'maps.googleapis.com',
+      '/maps/api/geocode/json',
+      {'address': address, 'key': 'AIzaSyBbKsb2PPKoMLl2-BLwIM9oeoXaFJEZQUg'},
+    );
+    final http.Response response = await http.get(uri);
+    final decodedResponse = json.decode(response.body);
+    final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+    final coordinates = decodedResponse['results'][0]['geometry']['location'];
+
     final StaticMapProvider staticMapProvider =
         StaticMapProvider('AIzaSyBbKsb2PPKoMLl2-BLwIM9oeoXaFJEZQUg');
     final Uri staticMapUri = staticMapProvider.getStaticUriWithMarkers(
-        [Marker('position', 'position', 41.40338, 2.17403)],
-        center: Location(41.40338, 2.17403),
+        [Marker('position', 'position', coordinates['lat'], coordinates['lng'])],
+        center: Location(coordinates['lat'], coordinates['lng']),
         width: 500,
         height: 300,
         maptype: StaticMapViewType.roadmap);
     setState(() {
+      _addressInputController.text = formattedAddress;
       _staticMapUri = staticMapUri;
     });
   }
@@ -51,6 +71,8 @@ class _LocationInputState extends State<LocationInput> {
           focusNode: _addressInputFocusNode,
           child: TextFormField(
             focusNode: _addressInputFocusNode,
+            controller: _addressInputController,
+            decoration: InputDecoration(labelText: 'Address'),
           ),
         ),
         SizedBox(height: 15),
