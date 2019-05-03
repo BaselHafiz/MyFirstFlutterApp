7fe1b8b769e86dd8bf1c98d4e98b1930f83e8571
Basel <basselhafiz@gmail.com>
2019-05-03 15:41:52 +0200

    15.8-10 Create 'storeImage' firebase cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const cors = require('cors')({ origin: true });
+const Busboy = require('busboy');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const fbAdmin = require('firebase-admin');
+const uuid = require('uuid/v4');
 
 /*
 
-Basel
+Basel - our firebase cloud function
 
 We'll create an API endpoint to accept incoming images and put them
 in the firebase storage and then we'll return links to these images
@@ -17,3 +17,88 @@ so that we can then store these links in the database and use them
 when we fetch our data from the database for displaying the images.
 
 */
+
+const gcconfig = {
+  projectId: 'my-first-flutter-app-c933e',
+  keyFilename: 'flutter-products.json'
+};
+
+const {Storage} = require('@google-cloud/storage');
+const gcs = new Storage(gcconfig);
+
+fbAdmin.initializeApp({
+  credential: fbAdmin.credential.cert(require('./flutter-products.json'))
+});
+
+exports.storeImage = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if (req.method !== 'POST') {
+      return res.status(500).json({ message: 'Not allowed.' });
+    }
+
+    if (
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith('Bearer ')
+    ) {
+      return res.status(401).json({ error: 'Unauthorized.' });
+    }
+
+    let idToken;
+    idToken = req.headers.authorization.split('Bearer ')[1];
+
+    const busboy = new Busboy({ headers: req.headers });
+    let uploadData;
+    let oldImagePath;
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      const filePath = path.join(os.tmpdir(), filename);
+      uploadData = { filePath: filePath, type: mimetype, name: filename };
+      file.pipe(fs.createWriteStream(filePath));
+    });
+
+    busboy.on('field', (fieldname, value) => {
+      oldImagePath = decodeURIComponent(value);
+    });
+
+    busboy.on('finish', () => {
+      const bucket = gcs.bucket('my-first-flutter-app-c933e.appspot.com');
+      const id = uuid();
+      let imagePath = 'images/' + id + '-' + uploadData.name;
+      if (oldImagePath) {
+        imagePath = oldImagePath;
+      }
+
+      return fbAdmin
+        .auth()
+        .verifyIdToken(idToken)
+        .then(decodedToken => {
+          return bucket.upload(uploadData.filePath, {
+            uploadType: 'media',
+            destination: imagePath,
+            metadata: {
+              metadata: {
+                contentType: uploadData.type,
+                firebaseStorageDownloadTokens: id
+              }
+            }
+          });
+        })
+        .then(() => {
+          return res.status(201).json({
+            imageUrl:
+              'https://firebasestorage.googleapis.com/v0/b/' +
+              bucket.name +
+              '/o/' +
+              encodeURIComponent(imagePath) +
+              '?alt=media&token=' +
+              id,
+            imagePath: imagePath
+          });
+        })
+        .catch(error => {
+          return res.status(401).json({ error: 'Unauthorized!' });
+        });
+    });
+    return busboy.end(req.rawBody);
+  });
+});
