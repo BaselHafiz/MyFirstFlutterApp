57d03444ff4a1b9363db3c5edf1b75e23f3b44dc
Basel <basselhafiz@gmail.com>
2019-07-05 19:42:21 +0200

    16.9. Sliding the Password-Confirm Field In & Out

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -22,11 +22,15 @@ class _AuthPageState extends State<AuthPage> with TickerProviderStateMixin {
   final TextEditingController _passwordTextController = TextEditingController();
   AuthMode _authMode = AuthMode.Login;
   AnimationController _controller;
+  Animation<Offset> _slideAnimation;
 
   @override
   void initState() {
     _controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _slideAnimation = Tween<Offset>(begin: Offset(0.0, -2.0), end: Offset.zero)
+        .animate(
+            CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
     super.initState();
   }
 
@@ -87,16 +91,19 @@ class _AuthPageState extends State<AuthPage> with TickerProviderStateMixin {
         parent: _controller,
         curve: Curves.easeIn,
       ),
-      child: TextFormField(
-        validator: (String value) {
-          if (_passwordTextController.text != value &&
-              _authMode == AuthMode.SignUp) {
-            return 'Password do not match';
-          }
-        },
-        obscureText: true,
-        keyboardType: TextInputType.text,
-        decoration: InputDecoration(labelText: 'Confirm Password'),
+      child: SlideTransition(
+        position: _slideAnimation,
+        child: TextFormField(
+          validator: (String value) {
+            if (_passwordTextController.text != value &&
+                _authMode == AuthMode.SignUp) {
+              return 'Password do not match';
+            }
+          },
+          obscureText: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(labelText: 'Confirm Password'),
+        ),
       ),
     );
   }
